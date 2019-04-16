b3add7241c7b9768c81e89e4dd06827003fb38dc
tacf <dev.tiago@gmail.com>
2019-04-16 09:56:22 +0100

    GPG conf + Fish + Brewfile.mac

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,16 @@
+function fish_prompt
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    echo -n (git-radar --fish --fetch)
+    set_color normal
+    echo -n ' > '
+end
+
+
+function fish_greeting
+	echo 'hmm, unresolved split-brain - 2018-06-08 16:44'
+end
+
 export GPG_TTY=(tty)
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
