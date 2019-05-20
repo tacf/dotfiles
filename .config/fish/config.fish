2ce32332966743fed6ea9026848ae18387593771
tacf <dev.tiago@gmail.com>
2019-05-20 11:14:19 +0100

    brew pkgs + it2 integration + fish conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,10 +16,10 @@ alias cat=bat
 alias dck "docker"
 
 # ALIAS AWS
-alias ec2-ls "aws ec2 describe-instances --query \"Reservations[*].Instances[*].{Name:Tags[?Key==`Name`].Value[] | [0], PublicIP:PublicIpAddress, Status:State.Name}\""
+alias ec2-ls "aws ec2 describe-instances --query \"Reservations[*].Instances[*].{Name:Tags[?Key==`Name`].Value[] | [0], PublicIP:PublicIpAddress, PrivateIP:PrivateIpAddress Status:State.Name}\""
 
 # Path
-set PATH $PATH $HOME/bin
+set PATH $PATH $HOME/bin $HOME/.cargo/bin
 
 # AUTH Tooling
 export GPG_TTY=(tty)
@@ -30,4 +30,7 @@ gpgconf --launch gpg-agent
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 status --is-interactive; and source (pyenv init -|psub)
 
+
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
